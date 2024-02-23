65cfc8e306b567d60de771f2181e6b0824874f14
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-02-23 15:05:49 -0300

    Emacs custom consult-fd function to list only files and not directories

--- config/consult.el
+++ config/consult.el
@@ -3,7 +3,18 @@
 (unless (package-installed-p 'consult)
   (package-install 'consult))
 
-(keymap-set evil-normal-state-map "C-q" 'consult-fd)
+(defun my/consult-fd-only-files ()
+  "Uses `consult-fd` with custom args set to type 'file only' on
+   current project-root or in the default-directory if root not found"
+  (interactive)
+  (let* ((current-root (project-root (project-current)))
+	 (default-directory (if current-root current-root default-directory))
+	 (consult-fd-args '((if (executable-find "fdfind" 'remote) "fdfind" "fd")
+			    "--full-path --color=never --type file")))
+    (call-interactively #'consult-fd)))
+
+(keymap-set evil-normal-state-map "C-q" #'my/consult-fd-only-files)
+
 (keymap-set evil-normal-state-map "C-f" 'consult-line)
 (keymap-set evil-normal-state-map "M-f" 'consult-line-multi)
 
