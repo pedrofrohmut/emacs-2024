6b044ad0b821a9a02dc36d6db9fe13818f9e0f84
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-04-25 12:34:34 -0300

    Compile command on SPC r r now works for projects and not just CWD

--- config/compile.el
+++ config/compile.el
@@ -10,21 +10,30 @@
 (evil-define-key 'normal compilation-mode-map (kbd "g n") 'compilation-next-error)
 (evil-define-key 'normal compilation-mode-map (kbd "g p") 'compilation-previous-error)
 
+(defun my/project-compile ()
+  (let ((default-directory (project-root (project-current t)))
+         (compilation-buffer-name-function
+           (or project-compilation-buffer-name-function
+             compilation-buffer-name-function)))
+    (compile compile-command)))
+
+(defun my/project-compile-saved-command ()
+  (interactive)
+  (if (string-empty-p compile-command)
+    (error "Compile command is empty")
+    (my/project-compile)))
+
 ;; Compile my code
 (keymap-set evil-normal-state-map "SPC r c" 'project-compile)
-(keymap-set evil-normal-state-map "SPC r r"
-  (lambda ()
-    (interactive)
-    (if (string-empty-p compile-command)
-      (error "Compile command is empty")
-      (compile compile-command))))
+(keymap-set evil-normal-state-map "SPC r r" #'my/project-compile-saved-command)
 
 ;; Hook that focus the Compilation window than jump the cursor to the first error
 (add-hook 'compilation-mode-hook
   (lambda ()
     (pop-to-buffer (get-buffer "*compilation*"))
-    ;; Add delay so the jump to error can work
-    (run-at-time 0.3 nil
+    ;; Add delay (in seconds: float) so the jump to error can work
+    ;; * doesnt work on slow compilations but is great for fast stuff
+    (run-at-time 1.0 nil
       (lambda ()
         (if (> compilation-num-errors-found 0)
           (compilation-next-error 1))))))
